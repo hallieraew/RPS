cafca8ce6e9a81d82663b2179b7c3588dd096ef7
hallieraew <hallieweintraub@gmail.com>
2020-01-27 19:08:40 -0600

    working submission to firebase

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,29 @@ var playerInput = ["Rock", "Paper", "Scissors"];
 var wins = 0;
 var loss = 0;
 var tie = 0;
+var player1 = "";
+var player2 = "";
 
-$(".start-button").on("click", function(){
-    $("#main-content").val("");
+var firebaseConfig = {
+    apiKey: "AIzaSyBKRA-loH0z9xQIrVIIK41PH2gRiNQrPhg",
+    authDomain: "rockpaperscissors-31fb0.firebaseapp.com",
+    databaseURL: "https://rockpaperscissors-31fb0.firebaseio.com",
+    projectId: "rockpaperscissors-31fb0",
+    storageBucket: "rockpaperscissors-31fb0.appspot.com",
+    messagingSenderId: "495768658180",
+    appId: "1:495768658180:web:1c61029e73cc47d4763051",
+    measurementId: "G-NJWCEK0ESS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-    console.log("clicked");
+  var database = firebase.database();
+
+  database.ref().set({
+    player1: player1, 
+    player2: player2
+});
+
 
     for (i=0; i<playerInput.length; i++) {
         var a = $("<button>");
@@ -23,57 +41,99 @@ $(".start-button").on("click", function(){
 
 
 // add in click function for choice of player 1
-// log choice to the db
-// when user clicks choice would then go to player2 screen - identical but replace player 1 with player 2
-// same click function would log choice to the DB
-database.ref().push({
-    player1: player1,
-    playerOneGuess: playerOneGuess,
-});
-// next screen is who won? click function to display who won, choices and tallies and then click function to bring back to player 1 choice page to start again
+
+$("#rock1").on("click", function(){
+    console.log("clicked");
+    player1 = "Rock";
+    // log choice to the db
+    // same click function would log choice to the DB
+    database.ref().set({
+        player1: player1
+    });
+})
+
+$("#paper1").on("click", function(){
+    console.log("clicked");
+    player1 = "Paper";
+    // log choice to the db
+    // same click function would log choice to the DB
+    database.ref().set({
+        player1: player1
+    });
+})
+
+$("#scissors1").on("click", function(){
+    console.log("clicked");
+    player1 = "Scissors";
+    // log choice to the db
+    // same click function would log choice to the DB
+    database.ref().set({
+        player1: player1
+    });
+})
+
+$("#rock2").on("click", function(){
+    console.log("clicked");
+    player2 = "Rock";
+    // log choice to the db
+    // same click function would log choice to the DB
+    database.ref().set({
+        player2: player2
+    });
+})
+
+$("#paper2").on("click", function(){
+    console.log("clicked");
+    player2 = "Paper";
+    // log choice to the db
+    // same click function would log choice to the DB
+    database.ref().set({
+        player2: player2
+    });
+})
+
+$("#scissors2").on("click", function(){
+    console.log("clicked");
+    player2 = "Scissors";
+    // log choice to the db
+    // same click function would log choice to the DB
+    database.ref().set({
+        player2: player2
+    });
+})
+
+
+$("#whoWonBtn").on("click", function() {
+    $("#mainContent").val("");
+    
+
 })
 
 
 
 
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBKRA-loH0z9xQIrVIIK41PH2gRiNQrPhg",
-    authDomain: "rockpaperscissors-31fb0.firebaseapp.com",
-    databaseURL: "https://rockpaperscissors-31fb0.firebaseio.com",
-    projectId: "rockpaperscissors-31fb0",
-    storageBucket: "rockpaperscissors-31fb0.appspot.com",
-    messagingSenderId: "495768658180",
-    appId: "1:495768658180:web:1c61029e73cc47d4763051",
-    measurementId: "G-NJWCEK0ESS"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-
 //  click function for who won? button{
 
 //   add in the if else stuff 
-if ((playerOneGuess === "r") || (playerOneGuess === "p") || (playerOneGuess === "s")) {
+// if ((player1 === "Rock") || (player1 === "Paper") || (player1 === "Scissors")) {
     
-    if ((playerOneGuess === "r" && playerTwoGuess === "s") ||
-      (playerOneGuess === "s" && playerTwoGuess === "p") || 
-      (playerOneGuess === "p" && playerTwoGuess === "r")) {
-      wins++;
-    } else if (playerOneGuess === playerTwoGuess) {
-      ties++;
-    } else {
-      losses++;
-    }
+//     if ((playerOneGuess === "r" && playerTwoGuess === "s") ||
+//       (playerOneGuess === "s" && playerTwoGuess === "p") || 
+//       (playerOneGuess === "p" && playerTwoGuess === "r")) {
+//       wins++;
+//     } else if (playerOneGuess === playerTwoGuess) {
+//       ties++;
+//     } else {
+//       losses++;
+//     }
 
-    // Hide the directions
-    directionsText.textContent = "";
 
-// display the wins losses 
-    $("#").text("Player 1 chose: " + playerOneGuess);
-    $("#").text("Player 2 chose: " + playerTwoGuess);
-    $("#").text("wins: " + wins);
-    $("#").text("losses: " + losses);
-    $("#").text("ties: " + ties);
+// // display the wins losses 
+//     $("#").text("Player 1 chose: " + playerOneGuess);
+//     $("#").text("Player 2 chose: " + playerTwoGuess);
+//     $("#").text("wins: " + wins);
+//     $("#").text("losses: " + losses);
+//     $("#").text("ties: " + ties);
     // reset(); havent created this function yet - it would go back to player 1 input screen so that they can keeo same game
-  }
+//   }
